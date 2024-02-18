72e8b1260684ae620f837f91e6f2919f4d9a9552
Archishman Sengupta <archishman1808@gmail.com>
2024-02-18 22:41:15 +0530

    feat: setup custom prompt, model loading and retrieval qa chain

--- model.py
+++ model.py
@@ -26,3 +26,58 @@ Please craft a clear and informative response that directly addresses the questi
 Aim for accuracy and relevance, keeping the user's needs in mind.
 Response:
 """
+
+# setting up a pipeline for question-answering (QA) retrieval
+def set_custom_prompt():
+  # Prompt template for QA retrieval for each vector stores
+  prompt = PromptTemplate(template = custom_prompt_template, input_variables = ['context','question'])
+  return prompt
+
+# loads a conditional transformer model for the QA retrieval task on llama-2-7b-chat-ggml
+def load_llama():
+  llm = CTransformers(
+    model_type = 'llama',
+    model = 'models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+    max_new_tokens = 512,
+    temperature = 0.5
+  )
+  return llm
+
+"""
+This method initializes a question-answering (QA) retrieval chain, which consists of a conditional
+    transformer model (CTransformers) for generating responses, a prompt template for constructing
+    prompts based on user queries, and a vector store (FAISS) for retrieving relevant documents
+    to support the QA process.
+
+    - llm (CTransformers): The conditional transformer model (CTransformers) plays a central role
+    in generating responses to user queries. It leverages pre-trained language
+    models to understand user questions and produce informative answers.
+
+    - prompt (PromptTemplate): The prompt template (PromptTemplate) defines the structure of prompts
+    provided to the QA model. It includes placeholders for the context
+    and question, guiding the model on how to interpret and respond to
+    user queries effectively.
+
+    - db (FAISS): The vector store (FAISS) serves as a repository of documents or passages that
+    the QA model can search through to find relevant information. It enables efficient
+    retrieval of documents based on the similarity of their embeddings to the user query.
+
+    The method initializes a RetrievalQA object, which orchestrates the interaction between the
+    conditional transformer model, prompt template, and vector store to facilitate QA retrieval.
+    The RetrievalQA object is configured with the specified components, including the model,
+    prompt template, retriever (vector store), and additional settings for controlling the QA process.
+
+    The initialized RetrievalQA object is returned, ready to be used for responding to user queries
+    by retrieving relevant information from the vector store and generating informative answers
+    using the conditional transformer model.
+"""
+def retrieval_qa_chain(llm, prompt, db):
+  qa_chain = RetrievalQA.from_chain_type(
+  llm = llm,
+  chain_type = 'stuff',
+  retriever = db.as_retriever(search_kwargs={'k':2}),
+  return_source_documents = True,
+  chain_type_kwargs = {'prompt':prompt},
+  )
+  return qa_chain
+
